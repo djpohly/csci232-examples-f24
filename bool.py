9e6b5d5b7dde8282fa61e8f86876be16b5e8d66a
Devin J. Pohly <djpohly@gmail.com>
2024-09-16 15:26:31 -0500

    new examples

--- bool.py
+++ bool.py
@@ -28,28 +28,9 @@ def all_same_type(xx):
 
 
 # Prime sieve
-sieve = [
-    False, False, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-    True, True, True, True, True,
-]
+primes = [True] * 100
+primes[0] = False
+primes[1] = False
 
 def print_true_indices(xx):
     i = 0
@@ -60,11 +41,38 @@ def print_true_indices(xx):
             pass
         i = i + 1
 
-def cross_off_multiples(xx, n):
-    i = 0
+def cross_off_multiples_old(xx, n):
+    i = n + 1
     while i < len(xx):
-        if xx[i] % n == 0:
-            if xx[i]
+        if i % n == 0:
+            xx[i] = False
+        i = i + 1
+
+def cross_off_multiples(xx, n):
+    i = n * 2
+    while i < len(xx):
+        xx[i] = False
+        i = i + n
+
+def sieve(xx):
+    p = 2
+    while p < len(xx):
+        if xx[p]:
+            cross_off_multiples(xx, p)
+        else:
+            pass
+        p = p + 1
+    print_true_indices(xx)
 
 
+fortytwo = [False, True, False, True, False, True]
 
+def binary_increment(bb):
+    i = 0
+    while i < len(bb) and bb[i]:
+        bb[i] = False
+        i = i + 1
+    if i >= len(bb):
+        bb.append(True)
+    else:
+        bb[i] = True
---
+++ listindex.py
@@ -0,0 +1,4 @@
+xx = [2, 3, 5, 7, 11]
+
+for x in xx:
+    print(x + 1)
